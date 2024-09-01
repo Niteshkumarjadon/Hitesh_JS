9049ecc4f9ffd8eb09180f5a3404e37d910ff08b
NiteshKumarJadon <niteshkumarjadon@gmail.com>
2024-09-01 11:27:50 +0530

    My First Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+      // console.log("Hello World");
+      // let name = 'Nitesh Kumar';
+      // console.log(name);
+
+      // // Camnot be a reserved keyword
+      // // Should be meaningful names
+      // // Can not start with a number (1name)
+
+      // let firstName = 'Nitesh';
+      // let FirstName = 'Nitesh';
+      // console.log(firstName);
+      // console.log(FirstName);
+
+      // let intrestRate = 0.3;
+      // intrestRate = 1;
+      // console.log(intrestRate);
+
+// let name = 'Mosh';
+// let age = 30;
+// let isApproved = false;
+// let firstName = undefined;
+// let selectedColor = null;
+
+// typeof name;
+// console.log(name);
+
+// let person = {name: 'Nitesh',
+//       age: 30
+// };
+
+// person.name = 'John';
+// console.log(person.name);
+confirm.log(this);
+
+const A = {
+      a: "Nitesh",
+      b: 123;
+      getelementbyID function(){
+            
+      }
+}
